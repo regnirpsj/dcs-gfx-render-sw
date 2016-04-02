67433a42f2ee87988424f5688a31bd948a583347
jan p springer <jsd@igroup.org>
2016-04-02 13:25:42 +0100

    fixed: frustum + d3d proj setup

--- src/app/projection/main.cpp
+++ src/app/projection/main.cpp
@@ -21,7 +21,9 @@
 
 // includes, project
 
+#include <glm/gtx/utilities.hpp>
 #include <hugh/platform/posix/application/base.hpp>
+#include <hugh/scene/object/camera/frustum.hpp>
 
 #include <../common/pipeline.hpp>
 
@@ -44,25 +46,39 @@ namespace {
     explicit app(application::command_line const& a)
       : inherited(a)
     {
+      TRACE("<unnamed>::app::app");
     }
 
     virtual signed run()
     {
-      glm::mat4 const                xform_proj(glm::frustum(-1.0, 1.0, -1.0, 1.0, 0.1, 100.0));
-      pipeline::base::viewport const vport(0, 0, 500, 500, 0, 1);
+      TRACE("<unnamed>::app::run");
       
-      glm::vec3 const p(0.5, 0.5, 0.5);
-      glm::vec3 const n(0.0, 1.0, 0.0);
+      using frustum = hugh::scene::object::camera::frustum;
+
+      frustum const                  f(-1.0, 1.0, -1.0, 1.0, 0.1, 100.0);
+      glm::mat4 const                xform_proj(glm::frustum(f.left, f.right,
+                                                             f.bottom, f.top,
+                                                             f.near, f.far));
+      pipeline::base::viewport const vport(0, 0, 100, 100, 0, 1);
+      
+      glm::vec3 const p(0.0, 0.0, -0.125);
+      glm::vec3 const n(0.0, 1.0,  0.0);
       
       std::cout << glm::io::precision(2) << glm::io::width(1 + 2 + 1 + 2);
       
-      pipeline::d3d const d3d(glm::mat4(), glm::mat4(), xform_proj, vport);
+      pipeline::d3d const d3d(glm::mat4(),
+                              glm::mat4(),
+                              glm::convert::opengl_to_d3d * xform_proj,
+                              vport);
 
       //std::cout << "d3d:"    << d3d                     << std::endl;
       std::cout << "d3d(p):" << d3d.transform(p, true)  << std::endl;
       std::cout << "d3d(n):" << d3d.transform(n, false) << std::endl;
 
-      pipeline::opengl const ogl(glm::mat4(), glm::mat4(), xform_proj, vport);
+      pipeline::opengl const ogl(glm::mat4(),
+                                 glm::mat4(),
+                                 xform_proj,
+                                 vport);
       
       //std::cout << "ogl:"    << ogl                     << std::endl;
       std::cout << "ogl(p):" << ogl.transform(p, true)  << std::endl;
