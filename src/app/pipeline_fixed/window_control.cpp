a78991c7d0cf2123fba59ba4320e7772de62b35b
jan p springer <jsd@igroup.org>
2016-04-23 22:15:54 +0100

    update

--- src/app/pipeline_fixed/window_control.cpp
+++ src/app/pipeline_fixed/window_control.cpp
@@ -28,6 +28,7 @@
 
 #include <gtkmm_wrap/check_button_input.hpp>
 #include <gtkmm_wrap/radio_button_input.hpp>
+#include <gtkmm_wrap/text_output.hpp>
 #include <gtkmm_wrap/utilities.hpp>
 
 #define HUGH_USE_TRACE
@@ -56,8 +57,8 @@ namespace {
 window_control::window_control()
   : hugh::gtkmm::window(),
     // viewport_          (0, 0, 80, 60, 0, 1),
-    viewport_          (0, 0, 320, 240, 0, 1),
-    // viewport_          (0, 0, 640, 480, 0, 1),
+    // viewport_          (0, 0, 320, 240, 0, 1),
+    viewport_          (0, 0, 640, 480, 0, 1),
     // viewport_          (0, 0, 1200, 900, 0, 1),
     pipeline_          (nullptr),
     win_color_         (nullptr),
@@ -67,7 +68,7 @@ window_control::window_control()
 
   {
     set_title             ("pipeline::fixed");
-    set_default_size      (250, 600);
+    set_default_size      (500, 700);
     set_border_width      (2);
     set_resizable         (true);
     set_reallocate_redraws(true);
@@ -145,16 +146,70 @@ window_control::window_control()
       }
     };
 
-    std::array<Gtk::Widget*, 2> const stats_ppl_item_list = {
+    static unsigned const stats_number_width(8);
+    
+    std::array<Gtk::Widget*, 1> const stats_vertex_item_list = {
       {
-        empty_label_in_framed_vbox("Vertices", ""),
-        empty_label_in_framed_vbox("Fragments", ""),
+        Gtk::manage(new text_output<std::string>
+                    ("  Transformed:",
+                     [&](){ return std::to_string((*pipeline_->stats).vertices.transformed); },
+                     "Number of transformed vertices.", stats_number_width)),
       }
     };
     
-    std::array<Gtk::Widget*, 1> const stats_item_list = {
+    std::array<Gtk::Widget*, 5> const stats_fragment_item_list = {
       {
-        pack_items_in_framed_hbox<2>("Pipeline", stats_ppl_item_list),
+        Gtk::manage(new text_output<std::string>
+                    ("      Created:",
+                     [&](){ return std::to_string((*pipeline_->stats).fragments.created); },
+                     "Number of created fragments.", stats_number_width)),
+        Gtk::manage(new text_output<std::string>
+                    ("     Z-Culled:",
+                     [&](){ return std::to_string((*pipeline_->stats).fragments.zculled); },
+                     "Number of z-culled fragments.", stats_number_width)),
+        Gtk::manage(new text_output<std::string>
+                    ("       Shaded:",
+                     [&](){ return std::to_string((*pipeline_->stats).fragments.shaded); },
+                     "Number of shaded fragments.", stats_number_width)),
+        Gtk::manage(new text_output<std::string>
+                    ("Z-Test Failed:",
+                     [&](){ return std::to_string((*pipeline_->stats).fragments.ztestfailed); },
+                     "Number of fragments where z-test failed.", stats_number_width)),
+        Gtk::manage(new text_output<std::string>
+                    ("      Updated:",
+                     [&](){ return std::to_string((*pipeline_->stats).fragments.updated); },
+                     "Number of fragments written to the frame buffer.", stats_number_width)),
+      }
+    };
+
+    std::array<Gtk::Widget*, 2> const stats_pipeline_item_list = {
+      {
+        pack_items_in_framed_vbox<1>("Vertices",  stats_vertex_item_list),
+        pack_items_in_framed_vbox<5>("Fragments", stats_fragment_item_list),
+      }
+    };
+    
+    std::array<Gtk::Widget*, 3> const stats_rasterizer_item_list = {
+      {
+        Gtk::manage(new text_output<std::string>
+                    ("Points:",
+                     [&](){ return std::to_string((*(*pipeline_->rasterizer)->stats).points); },
+                     "Number of rasterized points.", stats_number_width)),
+        Gtk::manage(new text_output<std::string>
+                    ("Lines:",
+                     [&](){ return std::to_string((*(*pipeline_->rasterizer)->stats).lines); },
+                     "Number of rasterized lines.", stats_number_width)),
+        Gtk::manage(new text_output<std::string>
+                    ("Triangles:",
+                     [&](){ return std::to_string((*(*pipeline_->rasterizer)->stats).triangles); },
+                     "Number of rasterized triangles.", stats_number_width)),
+      }
+    };
+    
+    std::array<Gtk::Widget*, 2> const stats_item_list = {
+      {
+        pack_items_in_framed_hbox<2>("Pipeline",   stats_pipeline_item_list),
+        pack_items_in_framed_hbox<3>("Rasterizer", stats_rasterizer_item_list),
       }
     };
 
@@ -191,7 +246,7 @@ window_control::window_control()
     std::array<Gtk::Widget*, 3> const top_level = {
       {
         pack_items_in_framed_vbox<2>("Control", control_item_list),
-        pack_items_in_framed_vbox<1>("Stats",   stats_item_list),
+        pack_items_in_framed_vbox<2>("Stats",   stats_item_list),
         pack_items_in_framed_vbox<1>("Output",  output_item_list),
       }
     };
@@ -215,7 +270,8 @@ window_control::render()
   TRACE("window_control::render");
 
   using namespace hugh::render::software;
-  
+
+  const_cast<rasterizer::base::statistics&>(*(*pipeline_->rasterizer)->stats).reset();
   const_cast<pipeline::base::statistics&>(*pipeline_->stats).reset();
   
   (*pipeline_->colorbuffer)->clear();
@@ -330,29 +386,55 @@ window_control::render()
     static std::array<glm::vec3 const, 9> const p = {
       {
         {  .0,  .0, .0625 }, // 0:  0  0
-        {  .5,  .0, .25 },   // 1: +x  0
-        {  .5,  .5, .95 },   // 2: +x +y
-        {  .0,  .5, .25 },   // 3:  0 +y
-        { -.5,  .5, .95 },   // 4: -x +y
-        { -.5,  .0, .25 },   // 5: -x  0
-        { -.5, -.5, .95 },   // 6: -x -y
-        {  .0, -.5, .25 },   // 7:  0 -y
-        {  .5, -.5, .95 },   // 8: +x -y
+        {  .3,  .0, .25 },   // 1: +x  0
+        {  .3,  .3, .95 },   // 2: +x +y
+        {  .0,  .3, .25 },   // 3:  0 +y
+        { -.3,  .3, .95 },   // 4: -x +y
+        { -.3,  .0, .25 },   // 5: -x  0
+        { -.3, -.3, .95 },   // 6: -x -y
+        {  .0, -.3, .25 },   // 7:  0 -y
+        {  .3, -.3, .95 },   // 8: +x -y
       }
     };
     
-    static std::array<vertex const, 24> const vertices = {
+    static std::array<glm::vec3 const, 4> const o = {
+      {
+        {  .05,  .05, .00 },
+        { -.05,  .05, .00 },
+        { -.05, -.05, .00 },
+        {  .05, -.05, .00 },
+      }
+    };
+    
+    static std::array<vertex const, 33> const vertices = {
       {
         // ccw
-        vertex(p[0], attr0), vertex(p[1], attr2), vertex(p[3], attr3),
-        vertex(p[0], attr0), vertex(p[3], attr3), vertex(p[5], attr2),
-        vertex(p[0], attr0), vertex(p[5], attr2), vertex(p[7], attr3),
-        vertex(p[0], attr0), vertex(p[7], attr3), vertex(p[1], attr2),
+        vertex(p[0]+o[0], attr0), vertex(p[1]+o[0], attr2), vertex(p[3]+o[0], attr3),
+        vertex(p[0]+o[1], attr0), vertex(p[3]+o[1], attr3), vertex(p[5]+o[1], attr2),
+        vertex(p[0]+o[2], attr0), vertex(p[5]+o[2], attr2), vertex(p[7]+o[2], attr3),
+        vertex(p[0]+o[3], attr0), vertex(p[7]+o[3], attr3), vertex(p[1]+o[3], attr2),
+        
+        vertex(glm::vec3( 0.65,  0.00, .001), attr1),
+        vertex(glm::vec3( 1.15, -1.15, .999), attr2),
+        vertex(glm::vec3( 1.15,  1.15, .999), attr3),
+
+        vertex(glm::vec3(-0.65,  0.00, .001), attr1),
+        vertex(glm::vec3(-1.15,  1.15, .999), attr3),
+        vertex(glm::vec3(-1.15, -1.15, .999), attr2),
+        
         // cw
-        vertex(p[0], attr0), vertex(p[3], attr3), vertex(p[1], attr2),
-        vertex(p[0], attr0), vertex(p[5], attr2), vertex(p[3], attr3),
-        vertex(p[0], attr0), vertex(p[7], attr3), vertex(p[5], attr2),
-        vertex(p[0], attr0), vertex(p[1], attr2), vertex(p[7], attr3),
+        vertex(p[0]+o[0], attr0), vertex(p[3]+o[0], attr3), vertex(p[1]+o[0], attr2),
+        vertex(p[0]+o[1], attr0), vertex(p[5]+o[1], attr2), vertex(p[3]+o[1], attr3),
+        vertex(p[0]+o[2], attr0), vertex(p[7]+o[2], attr3), vertex(p[5]+o[2], attr2),
+        vertex(p[0]+o[3], attr0), vertex(p[1]+o[3], attr2), vertex(p[7]+o[3], attr3),
+
+        vertex(glm::vec3( 0.65,  0.00, .001), attr1),
+        vertex(glm::vec3( 1.15,  1.15, .999), attr2),
+        vertex(glm::vec3( 1.15, -1.15, .999), attr3),
+
+        //vertex(glm::vec3(-0.65,  0.00, .001), attr1),
+        //vertex(glm::vec3(-1.15, -1.15, .999), attr3),
+        //vertex(glm::vec3(-1.15,  1.15, .999), attr2),
       }
     };
 
